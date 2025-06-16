5978535a71f27beef99e2b272803c3bda6c635a0
Balthazar Picsou <aurelien.strimelle@bstorm.be>
2025-06-16 16:19:57 +0200

    Endpoints for author 🐑

---
+++ DemoCleanArchitecture.Presentation.WebAPI/Controllers/AuthorController.cs
@@ -0,0 +1,75 @@
+﻿using DemoCleanArchitecture.ApplicationCore.Interfaces.Services;
+using DemoCleanArchitecture.Domain.Exceptions;
+using DemoCleanArchitecture.Domain.Modeles;
+using DemoCleanArchitecture.Presentation.WebAPI.Dto.Input;
+using DemoCleanArchitecture.Presentation.WebAPI.Dto.Output;
+using DemoCleanArchitecture.Presentation.WebAPI.Mappers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoCleanArchitecture.Presentation.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class AuthorController : ControllerBase
+    {
+        private readonly IAuthorService _authorService;
+        public AuthorController(IAuthorService authorService)
+        {
+            _authorService = authorService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<AuthorOutputDTO>))]
+        public IActionResult GetAll()
+        {
+            // TODO Ajouter des QueryParams -> Page/NbElement
+            IEnumerable<Author> authors = _authorService.GetAll(1, 20);
+
+            return Ok(authors.Select(a => a.ToDTO()));
+
+            // Utilisation du mapper alternative ↓
+            // return Ok(authors.Select(AuthorMapper.ToDTO));
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(AuthorDetailOutputDTO))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetById([FromRoute] long id)
+        {
+            try
+            {
+                Author author = _authorService.GetById(id);
+                return Ok(author.ToDetailDTO());
+            }
+            catch (AuthorNotFoundException e)
+            {
+                return NotFound(new { message = e.Message });
+            }
+        }
+
+        [HttpPost]
+        [Consumes("application/json")]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(AuthorDetailOutputDTO))]
+        public IActionResult Create([FromBody] AuthorInputDTO author)
+        {
+            Author authorCreated = _authorService.Create(author.ToModel());
+
+            return CreatedAtAction(
+                nameof(GetById), 
+                new { id = authorCreated.Id }, 
+                authorCreated.ToDetailDTO()
+            );
+        }
+
+        [HttpPut("{id}")]
+        [Consumes("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(AuthorDetailOutputDTO))]
+        public IActionResult Update([FromRoute] long id, [FromBody] AuthorInputDTO author)
+        {
+            // TODO Implement this methode :o
+            return StatusCode(StatusCodes.Status501NotImplemented);
+        }
+    }
+}
---
+++ DemoCleanArchitecture.Presentation.WebAPI/Mappers/AuthorMapper.cs
@@ -0,0 +1,42 @@
+﻿using DemoCleanArchitecture.Domain.Modeles;
+using DemoCleanArchitecture.Presentation.WebAPI.Dto.Input;
+using DemoCleanArchitecture.Presentation.WebAPI.Dto.Output;
+
+namespace DemoCleanArchitecture.Presentation.WebAPI.Mappers
+{
+    public static class AuthorMapper
+    {
+        public static AuthorOutputDTO ToDTO(this Author author)
+        {
+            return new AuthorOutputDTO()
+            {
+                Id = author.Id,
+                Name = $"{author.FirstName} {author.LastName}",
+                Pseudo = author.Pseudo,
+            };
+        }
+
+        public static AuthorDetailOutputDTO ToDetailDTO(this Author author)
+        {
+            return new AuthorDetailOutputDTO()
+            {
+                Id = author.Id,
+                Name = $"{author.FirstName} {author.LastName}",
+                Pseudo = author.Pseudo,
+                BirthDate = author.BirthDate?.ToString("O")
+            };
+        }
+
+        public static Author ToModel(this AuthorInputDTO authorInput)
+        {
+            return new Author()
+            {
+                FirstName = authorInput.FirstName,
+                LastName = authorInput.LastName,
+                Pseudo = authorInput.Pseudo,
+                BirthDate = authorInput.BirthDate
+            };
+        }
+    }
+}
+
