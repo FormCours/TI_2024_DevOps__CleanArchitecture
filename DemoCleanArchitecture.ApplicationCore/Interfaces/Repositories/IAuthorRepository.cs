a65dc726dbb34b610a367e39f21aa0c77d291e94
Balthazar Picsou <aurelien.strimelle@bstorm.be>
2025-06-16 11:45:25 +0200

    Author interfaces 🐍

---
+++ DemoCleanArchitecture.ApplicationCore/Interfaces/Repositories/IAuthorRepository.cs
@@ -0,0 +1,13 @@
+﻿using DemoCleanArchitecture.Domain.Modeles;
+
+namespace DemoCleanArchitecture.ApplicationCore.Interfaces.Repositories
+{
+    public interface IAuthorRepository
+    {
+        IEnumerable<Author> GetAll(int offset, int limit);
+        Author? GetById(long id);
+        Author Create(Author data);
+        Author Update(long id, Author data);
+        bool Delete(long id);
+    }
+}
