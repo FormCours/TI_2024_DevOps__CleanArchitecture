625bd4f167994cd76f098649d66683142742a0b5
Balthazar Picsou <aurelien.strimelle@bstorm.be>
2025-06-16 17:12:54 +0200

    Initialize EFCore 🦖

---
+++ DemoCleanArchitecture.Infrastructure.Database/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using DemoCleanArchitecture.Domain.Modeles;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace DemoCleanArchitecture.Infrastructure.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Author> Author { get; set; }
+
+
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
