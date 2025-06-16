0fb66cf86d8c5cfadcac462c774b8e5b79210a30
Balthazar Picsou <aurelien.strimelle@bstorm.be>
2025-06-16 11:25:22 +0200

    Author models 🐁

---
+++ DemoCleanArchitecture.Domain/Modeles/Author.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoCleanArchitecture.Domain.Modeles
+{
+    public class Author
+    {
+        public long Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string? Pseudo { get; set; }
+        public required DateTime? BirthDate { get; set; }
+    }
+}
