c1a5aa20032c9c807226ddf6114385522b1075ca
Balthazar Picsou <aurelien.strimelle@bstorm.be>
2025-06-16 14:18:44 +0200

    Implement of author service 🫎

---
+++ DemoCleanArchitecture.Domain/Exceptions/NotFoundException.cs
@@ -0,0 +1,16 @@
+﻿namespace DemoCleanArchitecture.Domain.Exceptions
+{
+    public class NotFoundException<TId> : Exception
+        where TId : struct
+    {
+        public TId Id { get; init; }
+        public NotFoundException(string subject, TId id)
+            : base($"The {subject} with id {id} is not found")
+        { }
+    }
+
+    public class AuthorNotFoundException : NotFoundException<long>
+    {
+        public AuthorNotFoundException(long id) : base("Author", id) { }
+    }
+}
