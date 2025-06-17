bf9226a26d576aee9be8595370e81869bb38af8e
Balthazar Picsou <aurelien.strimelle@bstorm.be>
2025-06-17 10:54:59 +0200

    Config Injection dependency 🐧

--- DemoCleanArchitecture.Presentation.WebAPI/Program.cs
+++ DemoCleanArchitecture.Presentation.WebAPI/Program.cs
@@ -1,9 +1,26 @@
+using DemoCleanArchitecture.ApplicationCore.Interfaces.Repositories;
+using DemoCleanArchitecture.ApplicationCore.Interfaces.Services;
+using DemoCleanArchitecture.ApplicationCore.Services;
+using DemoCleanArchitecture.Infrastructure.Database;
+using DemoCleanArchitecture.Infrastructure.Database.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+// - ApplicationCore
+builder.Services.AddScoped<IAuthorService, AuthorService>();
+// - Infrastructure
+builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
+// - DB Context
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+// - Controllers
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
